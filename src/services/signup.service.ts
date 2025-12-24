4731911f580781d7470e1786e6016c5d3644ddb8
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-12-24 13:34:22 -0300

    feat: sign up services

---
+++ src/services/signup.service.ts
@@ -0,0 +1,38 @@
+import React from "react";
+import { UserDTO, UserSignUp } from "../../types/user.type";
+import api from "./api";
+import { modalTheme } from "@/utils/modal-theme";
+import { ModalType } from "@/utils/enum.util";
+
+export async function handleForm(e: React.FormEvent, form: UserSignUp, arrayUsers: UserDTO[], setModalProps: React.Dispatch<React.SetStateAction<any>>, setIsModalOpen: React.Dispatch<React.SetStateAction<any>>) {
+
+    e.preventDefault();
+
+    if (form.password.trim() == form.confirmPassword.trim()) {
+
+        const emailExists = arrayUsers.some((u: UserDTO) => u.email.trim() === form.email.trim());
+
+        if (emailExists) {
+
+            setIsModalOpen(true);
+            setModalProps({ phrase: "Email already signed up on the plataform!", modalType: ModalType.ERROR, text: modalTheme.error.text, border: modalTheme.error.border });
+        } else {
+
+            const user = await api.post("/user", form);
+
+            if (!user) {
+
+                setIsModalOpen(true);
+                setModalProps({ phrase: "Failed to sign up user!", modalType: ModalType.ERROR, text: modalTheme.error.text, border: modalTheme.error.border });
+            };
+
+            setIsModalOpen(true);
+            setModalProps({ phrase: "You have signed up with success!", modalType: ModalType.SUCCESS, text: modalTheme.success.text, border: modalTheme.success.border });
+        };
+
+    } else {
+
+        setIsModalOpen(true);
+        setModalProps({ phrase: "Password must be the same as confirm password!", modalType: ModalType.ERROR, text: modalTheme.error.text, border: modalTheme.error.border });
+    };
+};
